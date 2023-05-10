6913d919c1d995d81678db19afc4a40a1bf18bae
ManiBecker <mani.becker@webde>
2023-05-11 00:20:15 +0200

    Initial Visual C# Project
    Creating initial Visual C# Project, adding background image and resize function

---
+++ MBLittleProfessor2/FormMain.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace MBLittleProfessor2
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.panelBackground = new System.Windows.Forms.Panel();
+            this.pictureBoxBackground = new System.Windows.Forms.PictureBox();
+            this.panelBackground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelBackground
+            // 
+            this.panelBackground.Controls.Add(this.pictureBoxBackground);
+            this.panelBackground.Location = new System.Drawing.Point(12, 12);
+            this.panelBackground.Name = "panelBackground";
+            this.panelBackground.Size = new System.Drawing.Size(200, 198);
+            this.panelBackground.TabIndex = 1;
+            // 
+            // pictureBoxBackground
+            // 
+            this.pictureBoxBackground.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBoxBackground.Image = global::MBLittleProfessor2.Properties.Resources.TI_Little_Professor_1991;
+            this.pictureBoxBackground.Location = new System.Drawing.Point(55, 16);
+            this.pictureBoxBackground.Name = "pictureBoxBackground";
+            this.pictureBoxBackground.Size = new System.Drawing.Size(91, 167);
+            this.pictureBoxBackground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxBackground.TabIndex = 1;
+            this.pictureBoxBackground.TabStop = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panelBackground);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "MBLittleProfessor2";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Resize += new System.EventHandler(this.FormMain_Resize);
+            this.panelBackground.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackground)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBackground;
+        private System.Windows.Forms.PictureBox pictureBoxBackground;
+
+    }
+}
+
---
+++ MBLittleProfessor2/FormMain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MBLittleProfessor2
+{
+    public partial class FormMain : Form
+    {
+        private float fRatio, fDx, fDy;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Resize(object sender, EventArgs e)
+        {
+            if ((int)(pictureBoxBackground.Width / fRatio) <= panelBackground.Height)
+            {
+                pictureBoxBackground.Width = panelBackground.Width;
+                pictureBoxBackground.Height = (int)(pictureBoxBackground.Width / fRatio);
+            }
+
+            if (pictureBoxBackground.Height >= panelBackground.Height)
+            {
+                pictureBoxBackground.Width = (int)(panelBackground.Height * fRatio);
+                pictureBoxBackground.Height = (int)(pictureBoxBackground.Width / fRatio);
+            }
+
+            fDx = (float)pictureBoxBackground.Width / (float)pictureBoxBackground.Image.Width;
+            fDy = (float)pictureBoxBackground.Height / (float)pictureBoxBackground.Image.Height;
+
+            pictureBoxBackground.Top = (panelBackground.Height - pictureBoxBackground.Height) / 2;
+            pictureBoxBackground.Left = (panelBackground.Width - pictureBoxBackground.Width) / 2;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            fRatio = (float)pictureBoxBackground.Image.Width / (float)pictureBoxBackground.Image.Height;
+            panelBackground.Dock = DockStyle.Fill;
+            FormMain_Resize(sender, e);
+        }
+    }
+}
